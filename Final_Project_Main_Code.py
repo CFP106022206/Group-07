a28189a74f111c474a9cda6ad8ad9c81f8fb18b4
CFP106020008 <48315222+CFP106020008@users.noreply.github.com>
2019-05-02 16:52:32 +0800

    Add files via upload

--- Final_Project_Main_Code.py
+++ Final_Project_Main_Code.py
@@ -21,7 +21,19 @@ R.astype('float64')
 R_datas = R[0:2,:] #儲存所有frame的位置資訊，是個"3*n*simulation_frame"的陣列
 
 #給定 Initial Condition
-#(待處理)
+#給定位置(均勻球形分布)
+r = 100 #小天體的半徑
+k = n
+while k > 0:
+    position = (np.random.random((1,3))-np.ones((1,3))*0.5)*2*r
+    norm = np.linalg.norm(position)
+    if norm < r:
+        R[0:3,n-k] = position
+        k -= 1
+#給定速度(等速前進)
+for i in range(0,n):
+    R[3:6,i] = np.array([1,0,0])
+
 
 
 def frame(R): #用於計算
@@ -31,9 +43,9 @@ def frame(R): #用於計算
 def store(R): #用於儲存不同時間的位置資訊
     new_R_data = frame(R)[0:2,:]
     np.append(R_datas,new_R_data,axis = 2)
-
+'''
 for i in range(0,simulation_frame): #執行主程式
     frame(R)
     store(R)
-
+'''
 #繪圖與輸出教給藝術總監 @陳重名
