2d1001b7f89996dd7e50be5e2bb41e8328248ed2
CFP106020008 <48315222+CFP106020008@users.noreply.github.com>
2019-05-02 19:56:13 +0800

    Add files via upload

--- Final_Project_Main_Code.py
+++ Final_Project_Main_Code.py
@@ -27,7 +27,11 @@ T = 0
 
 R = np.ones((8,n))
 R.astype('float64')
-R_datas = R[0:2,:] #儲存所有frame的位置資訊，是個"3*n*simulation_frame"的陣列
+
+global R_datas
+R_datas = np.zeros((3,n,simulation_frame))
+R_datas[0:3,:,0] = R[0:3,:] #儲存所有frame的位置資訊，是個"3*n*simulation_frame"的陣列
+
 
 #給定 Initial Condition
 #給定位置(均勻球形分布)
@@ -44,20 +48,25 @@ for i in range(0,n):
     R[3:6,i] = np.array([1,0,0])
 
 
-
+'''
 def frame(R): #用於計算
-    empty_array = np.zeros((8,n)
+    empty_array = np.zeros((8,n))
     test.main_func(R,empty_array,G,time_resoultion,e)
     R = empty_array
-    return R, dt #回傳dt以便計算總時間
+    return R 
+'''
+def frame(R):
+    return R
 
-def store(R): #用於儲存不同時間的位置資訊
-    new_R_data = frame(R)[0:2,:]
-    np.append(R_datas,new_R_data,axis = 2)
+def store(R,t): #用於儲存不同時間的位置資訊
+    new_R_data = frame(R)[0:3,:]
+    global R_datas
+    R_datas[0:3,:,t] = new_R_data
 
 
-for i in range(0,simulation_frame): #執行主程式
+
+for t in range(0,simulation_frame): #執行主程式
     frame(R)
-    store(R)
+    store(R,t)
 
 #繪圖與輸出教給藝術總監 @陳重名
