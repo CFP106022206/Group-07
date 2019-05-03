660d922844f256f6e2efdae2777dd1c8cac7717b
CFP106020008 <48315222+CFP106020008@users.noreply.github.com>
2019-05-03 19:29:25 +0800

    Add files via upload

--- Final_Project_Main_Code.py
+++ Final_Project_Main_Code.py
@@ -9,6 +9,7 @@ import numpy as np #計算用
 import matplotlib.pyplot as plt #畫圖用
 import imageio as io #輸出影片用
 import test
+from mpl_toolkits.mplot3d import Axes3D
 
 global G #Gravitational constant
 G = 1 
@@ -25,7 +26,6 @@ e = 0
 global T
 T = 0
 
-global R
 R = np.ones((n,8))
 R.astype('float64')
 
@@ -39,7 +39,7 @@ R_datas[:,0:3,0] = R[:,0:3] #儲存所有frame的位置資訊，是個"3*n*simul
 r = 100 #小天體的半徑
 k = n
 while k > 0:
-    position = np.transpose((np.random.random((3,1))-np.ones((3,1))*0.5)*2*r)
+    position = np.transpose((np.random.random((3,1))-np.ones((3,1))*500)*2*r)
     norm = np.linalg.norm(position)
     if norm < r:
         R[n-k,0:3] = position
@@ -51,11 +51,11 @@ for i in range(0,n):
 
 def frame(): #用於計算
     global R
-    empty_array = np.empty_like(R)
+    empty_array = np.zeros((8,n))
     empty_array = empty_array.copy(order='C')
     R = R.copy(order='C')
     test.main_func(R,empty_array,G,time_resoultion,e)
-    R = empty_array
+    R = np.transpose(empty_array)
 
 
 def store(t): #用於儲存不同時間的位置資訊
@@ -66,5 +66,22 @@ def store(t): #用於儲存不同時間的位置資訊
 for t in range(0,simulation_frame): #執行主程式
     frame()
     store(t)
+
+def image(i):
+    fig, ax = plt.subplots(figsize=(5,5))
+    ax.scatter(R_datas[:,0,i],R_datas[:,1,i])
+    ax.set(xlabel='Offset', ylabel='Offset',title='frame:{}'.format(t))
+    ax.set_ylim(-1000, 1000)
+    ax.set_xlim(-1000, 1000)
+    
+    fig.canvas.draw()  # draw the canvas, cache the renderer
+    image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+    image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    
+    return image
+
+kwargs_write = {'fps':24.0, 'quantizer':'nq'}
+io.mimsave('./movie.mp4', [image(i) for i in range(simulation_frame)], fps=20)
+
 
 #繪圖與輸出教給藝術總監 @陳重名
