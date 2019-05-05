e057ceab336bf7d841a9b1b4281b72f48fbce470
CFP106022212 <48315247+CFP106022212@users.noreply.github.com>
2019-05-05 16:32:20 +0800

    Update Final_Project_Main_Code.py

--- Final_Project_Main_Code.py
+++ Final_Project_Main_Code.py
@@ -6,67 +6,69 @@ Created on Thu Apr 25 17:07:06 2019
 """
 
 import numpy as np #計算用
-import matplotlib.pyplot as plt #畫圖用
-import imageio as io #輸出影片用
+#import matplotlib.pyplot as plt #畫圖用
+#import imageio as io #輸出影片用
 import test
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 
-global G #Gravitational constant
-G = 1 
-global M #Center Body Mass
-M = 5000
-global n #Number of particles
-n = 100 
-global simulation_frame
-simulation_frame = 100
-global time_resoultion
+G = 6.67*10**-11 #Gravitational constant
+ 
+M = 8000         #Center Body Mass
+
+n = 100          #Number of particles
+
+simulation_frame = 10
 time_resoultion = 1
-global e #coefficient of restitution
-e = 0
-global T
+
+e = 0            #coefficient of restitution
+
 T = 0
 
 R = np.ones((n,8))
 R.astype('float64')
-
-global R_datas
-R_datas = np.zeros((n,3,simulation_frame))
-R_datas[:,0:3,0] = R[:,0:3] #儲存所有frame的位置資訊，是個"3*n*simulation_frame"的陣列
-
+R = R.copy(order='C')
+ 
+empty_array = np.empty_like(R)
+empty_array = empty_array.copy(order='C')
 
 #給定 Initial Condition
 #給定位置(均勻球形分布)
 r = 100 #小天體的半徑
 k = n
 while k > 0:
-    position = np.transpose((np.random.random((3,1))-np.ones((3,1))*500)*2*r)
+    position = (np.random.random((1,3))-np.ones((1,3)))*2*r
     norm = np.linalg.norm(position)
     if norm < r:
         R[n-k,0:3] = position
         k -= 1
 #給定速度(等速前進)
-for i in range(0,n):
-    R[i,3:6] = np.array([1,0,0])
+R[:,4:6] *= 0
+R[:,6]   *= 8000
+
+R_datas = np.zeros((simulation_frame,n,3))
+R_datas[0,:,:3] = R[:,:3] #儲存所有frame的位置資訊
 
 
 def frame(): #用於計算
+    global G
+    global time_resoultion
     global R
-    empty_array = np.zeros((8,n))
-    empty_array = empty_array.copy(order='C')
-    R = R.copy(order='C')
+    global e
+    global empty_array 
+    
     test.main_func(R,empty_array,G,time_resoultion,e)
-    R = np.transpose(empty_array)
+    R = empty_array
 
 
 def store(t): #用於儲存不同時間的位置資訊
-    new_R_data = R[:,0:3]
     global R_datas
-    R_datas[:,0:3,t] = new_R_data
+    global R
+    R_datas[t,:,:3] = R[:,:3]
 
-for t in range(0,simulation_frame): #執行主程式
+for t in range(1,simulation_frame): #執行主程式
     frame()
     store(t)
-
+'''
 def image(i):
     fig, ax = plt.subplots(figsize=(5,5))
     ax.scatter(R_datas[:,0,i],R_datas[:,1,i])
@@ -82,6 +84,7 @@ def image(i):
 
 kwargs_write = {'fps':24.0, 'quantizer':'nq'}
 io.mimsave('./movie.mp4', [image(i) for i in range(simulation_frame)], fps=20)
+'''
 
 
 #繪圖與輸出教給藝術總監 @陳重名
