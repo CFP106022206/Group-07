56d00f966b990ad75e6e1a0edfa3310b2b90051e
CFP106020008 <48315222+CFP106020008@users.noreply.github.com>
2019-05-08 01:24:12 +0800

    Add files via upload

--- mp4_maker.py
+++ mp4_maker.py
@@ -7,15 +7,28 @@ Created on Mon May  6 21:25:19 2019
 
 import imageio #Making videos
 import os
+import time
+
+start = time.time()
 
 fileList = []
-for file in os.listdir('C:/Users/林彥興/.spyder-py3/Computation of physics/Final Project/frame/'):
+for file in os.listdir('C:/Users/林彥興/.spyder-py3/Computation of physics/Final Project/frame_Earth_far/'):
     if file.startswith('Roche_limit_'):
-        complete_path = 'C:/Users/林彥興/.spyder-py3/Computation of physics/Final Project/frame/' + file
+        complete_path = 'C:/Users/林彥興/.spyder-py3/Computation of physics/Final Project/frame_Earth_far/' + file
         fileList.append(complete_path)
 
-writer = imageio.get_writer('test.mp4', fps=20)
+writer = imageio.get_writer('Roche_limit_Earth_circular.mp4', fps=20)
+
+t = 0
 
 for im in fileList:
     writer.append_data(imageio.imread(im))
+    t += 1
+    print(t)
+
 writer.close()
+
+end = time.time()
+
+print('Saving time: ' + str(end-start))
+
